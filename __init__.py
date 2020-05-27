25bcad5508367a76beed411c4761fb33697dd3c5
Andrew Peel <blenderpro3d@gmail.com>
2020-05-26 17:26:29 -0700

    Update Version Number

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ from bpy.app.handlers import persistent
 bl_info = {
     "name": "Home Builder Library",
     "author": "Andrew Peel",
-    "version": (0, 0, 1),
+    "version": (0, 0, 2),
     "blender": (2, 80, 0),
     "location": "Asset Library",
     "description": "Library designed to help with architectural and interior design",
