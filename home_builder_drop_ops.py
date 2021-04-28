7f3ed5c4acb6e0a1a2b0afdb49379d4898c41c85
Andrew Peel <34818078+CreativeDesigner3D@users.noreply.github.com>
2021-04-28 15:46:09 -0700

    Adding Place Decorative Asset Operator

--- home_builder_drop_ops.py
+++ home_builder_drop_ops.py
@@ -120,7 +120,7 @@ class home_builder_OT_drop(Operator):
                 obj_bp = self.get_custom_cabinet(context,os.path.join(directory,filename + ".blend"))
                 bpy.ops.home_builder.move_cabinet(obj_bp_name=obj_bp.name)
             if props.kitchen_tabs == 'DECORATIONS':
-                pass
+                bpy.ops.home_builder.place_decoration(filepath=self.filepath)  
 
         if props.library_tabs == 'BATHS':
             if props.bath_tabs == 'FIXTURES':
@@ -130,7 +130,7 @@ class home_builder_OT_drop(Operator):
             if props.bath_tabs == 'MIRRORS':
                 pass
             if props.bath_tabs == 'DECORATIONS':
-                pass                    
+                bpy.ops.home_builder.place_decoration(filepath=self.filepath)                    
 
         if props.library_tabs == 'CLOSETS':
             if props.closet_tabs == 'FLOOR_PANELS':
@@ -1719,6 +1719,112 @@ class home_builder_OT_place_wall_obstacle(bpy.types.Operator):
         return {'FINISHED'}
 
 
+class home_builder_OT_place_decoration(bpy.types.Operator):
+    bl_idname = "home_builder.place_decoration"
+    bl_label = "Place Decoration"
+    
+    filepath: bpy.props.StringProperty(name="Filepath",default="Error")
+
+    obj_bp_name: bpy.props.StringProperty(name="Obj Base Point Name")
+
+    current_wall = None
+
+    starting_point = ()
+
+    parent_obj_dict = {}
+    all_objects = []
+
+    def reset_properties(self):
+        self.current_wall = None
+        self.starting_point = ()
+        self.parent_obj_dict = {}
+        self.all_objects = []
+
+    def execute(self, context):
+        self.reset_properties()
+        self.create_drawing_plane(context)
+        self.get_object(context)
+        context.window_manager.modal_handler_add(self)
+        context.area.tag_redraw()
+        return {'RUNNING_MODAL'}
+
+    def get_object(self,context):
+        path, ext = os.path.splitext(self.filepath)
+        object_file_path = os.path.join(path + ".blend")
+        with bpy.data.libraries.load(object_file_path, False, False) as (data_from, data_to):
+                data_to.objects = data_from.objects
+        for obj in data_to.objects:
+            obj.display_type = 'WIRE'
+            self.all_objects.append(obj)
+            if obj.parent is None:
+                self.parent_obj_dict[obj] = (obj.location.x, obj.location.y, obj.location.z)            
+            context.view_layer.active_layer_collection.collection.objects.link(obj)  
+
+    def set_placed_properties(self,obj):
+        if obj.type == 'MESH' and obj.hide_render == False:
+            obj.display_type = 'TEXTURED'          
+        for child in obj.children:
+            self.set_placed_properties(child) 
+
+    def modal(self, context, event):
+        bpy.ops.object.select_all(action='DESELECT')
+
+        context.view_layer.update()
+        self.mouse_x = event.mouse_x
+        self.mouse_y = event.mouse_y
+
+        selected_point, selected_obj, selected_normal = pc_utils.get_selection_point(context,event,exclude_objects=self.all_objects)
+
+        self.position_object(selected_point,selected_obj)
+
+        if event_is_place_asset(event):
+            return self.finish(context)
+            
+        if event_is_cancel_command(event):
+            return self.cancel_drop(context)
+
+        if event_is_pass_through(event):
+            return {'PASS_THROUGH'}
+
+        return {'RUNNING_MODAL'}
+
+    def position_object(self,selected_point,selected_obj):
+        for obj, location in self.parent_obj_dict.items():
+            obj.location = selected_point
+            obj.location.x += location[0]
+            obj.location.y += location[1]
+            obj.location.z += location[2]
+
+    def cancel_drop(self,context):
+        obj_list = []
+        obj_list.append(self.drawing_plane)
+        for obj in self.all_objects:
+            obj_list.append(obj)
+        pc_utils.delete_obj_list(obj_list)
+        return {'CANCELLED'}
+
+    def create_drawing_plane(self,context):
+        bpy.ops.mesh.primitive_plane_add()
+        plane = context.active_object
+        plane.location = (0,0,0)
+        self.drawing_plane = context.active_object
+        self.drawing_plane.display_type = 'WIRE'
+        self.drawing_plane.dimensions = (100,100,1)
+
+    def finish(self,context):
+        context.window.cursor_set('DEFAULT')
+        bpy.ops.object.select_all(action='DESELECT')
+        if self.drawing_plane:
+            pc_utils.delete_obj_list([self.drawing_plane])
+        bpy.ops.object.select_all(action='DESELECT')
+        for obj, location in self.parent_obj_dict.items():
+            obj.select_set(True)  
+            context.view_layer.objects.active = obj     
+        for obj in self.all_objects:
+            self.set_placed_properties(obj) 
+        context.area.tag_redraw()
+        return {'FINISHED'}
+
 classes = (
     home_builder_OT_drop,
     home_builder_OT_place_cabinet,
@@ -1726,6 +1832,7 @@ classes = (
     home_builder_OT_place_closet,
     home_builder_OT_place_closet_insert,
     home_builder_OT_place_wall_obstacle,
+    home_builder_OT_place_decoration,
 )
 
 register, unregister = bpy.utils.register_classes_factory(classes)
